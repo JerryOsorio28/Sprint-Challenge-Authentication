ba7db4489996a9c312474a1f7b7e42dc296011c9
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-09-20 12:15:48 -0400

    User is able to register/login, with valid token is able to fetch jokes

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,47 @@
-const router = require('express').Router();
+const router = require('express').Router(); //import Express Router
+const Users = require('./auth-helpers.js'); //import helpers/model
+const bcrypt = require('bcryptjs'); //import bcrypt dependency
+const jwt = require('jsonwebtoken'); // import JSON web token dependency
+const { generateToken }  = require('./generateToken.js')//import function to generate user token
+
+//<-----Added GET request to validate generated token
+router.get('/')
 
 router.post('/register', (req, res) => {
   // implement registration
-});
+    const { username, password } = req.body; //fetch data from body
 
+    const hash = bcrypt.hashSync(password, 12) //Creates hash
+
+    Users.addUser({ username, password: hash })
+      .then(user => {
+        console.log(user)
+        res.status(201).json({
+          message: 'You have been sucessfully registered!'
+        })
+      })
+      .catch(err => res.status(500).json({error: err.response}))
+});
+ 
 router.post('/login', (req, res) => {
   // implement login
+  const { username, password } = req.body;
+
+    Users.getUser({ username })
+      .then(user => {
+        if(user && bcrypt.compareSync(password, user.password)){
+          const token = generateToken(user);
+          res.status(200).json({
+            message: `Welcome ${user.username}!`,
+            token: token
+          })
+        } else {
+          res.status(404).json({
+            message: `User ${user.username} not found`
+          })
+        }
+      })
+      .catch(err => res.status(500).json({error: err.response}))
 });
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,24 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require('jsonwebtoken');
+const secrets = require('./secrets.js');
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+
+  const token = req.headers.authorization; // fetching token from the header
+    if(token){
+      jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+        if(err) {
+          //token is expired
+          res.status(401).json({ message: 'Invalid credentials' }) 
+        } else {
+          //token is still valid
+          next();
+          req.user = { username: decodedToken.username }
+        }
+      })
+    } else {
+      res.status(401).json({ you: 'shall not pass!' });
+    }
 };
